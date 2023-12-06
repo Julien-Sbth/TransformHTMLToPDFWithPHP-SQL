e7ec3c48d0a1c0378e1317891f2abb6d5668a38b
JS <julien.sabathe@ynov.com>
2023-12-06 23:05:10 +0100

    [ADD Div Class For CSS Selector]

--- index.php
+++ index.php
@@ -1,38 +1,47 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
     <title>Formulaire</title>
     <link rel="stylesheet" href="/index.css">
 </head>
 <body>
-<form action="traitement.php" method="post">
-    <label for="prenom">Prénom :</label>
-    <input type="text" id="prenom" name="prenom"><br>
-
-    <label for="nom">Nom :</label>
-    <input type="text" id="nom" name="nom"><br>
-
-    <label for="telephone">Téléphone :</label>
-    <input type="text" id="telephone" name="telephone"><br>
-
-    <label for="pays">Pays :</label>
-    <input type="text" id="pays" name="pays"><br>
-
-    <label for="adresse">Adresse :</label>
-    <input type="text" id="adresse" name="adresse"><br>
-
-    <label for="email">Email :</label>
-    <input type="text" id="email" name="email"><br>
-
-    <label for="competence">Compétence :</label>
-    <textarea id="competence" name="competence" wrap="soft"></textarea>
-
-    <label for="experience">Experience :</label>
-    <textarea id="experience" name="experience" wrap="soft"></textarea>
-
-    <label for="ville">Ville :</label>
-    <input type="text" id="ville" name="ville"><br>
-
+<form action="traitement.php" method="post" class="form-grid">
+    <div class="input-group">
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" value="Prénom">
+    </div>
+    <div class="input-group">
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" value="Nom">
+    </div>
+    <div class="input-group">
+        <label for="telephone">Téléphone :</label>
+        <input type="text" id="telephone" name="telephone" value="Téléphone">
+    </div>
+    <div class="input-group">
+        <label for="pays">Pays :</label>
+        <input type="text" id="pays" name="pays" value="Pays">
+    </div>
+    <div class="input-group">
+        <label for="adresse">Adresse :</label>
+        <input type="text" id="adresse" name="adresse" value="adresse">
+    </div>
+    <div class="input-group">
+        <label for="email">Email :</label>
+        <input type="text" id="email" name="email" value="email">
+    </div>
+    <div class="input-group">
+        <label for="competence">Compétence :</label>
+        <textarea id="competence" name="competence" wrap="soft"></textarea>
+    </div>
+    <div class="input-group">
+        <label for="experience">Experience :</label>
+        <textarea id="experience" name="experience" wrap="soft"></textarea>
+    </div>
+    <div class="input-group">
+        <label for="ville">Ville :</label>
+        <input type="text" id="ville" name="ville" value="Ville">
+    </div>
     <input type="submit" value="Envoyer">
 </form>
 </body>
