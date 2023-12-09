9bda415479dfba0603b0c925519acf6c631e15fb
JS <julien.sabathe@ynov.com>
2023-12-09 18:46:40 +0100

    [Update ADd Placeholder]

--- index.php
+++ index.php
@@ -8,39 +8,39 @@
 <form action="traitement.php" method="post" class="form-grid">
     <div class="input-group">
         <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom" value="Prénom">
+        <input type="text" id="prenom" name="prenom" placeholder="Prénom">
     </div>
     <div class="input-group">
         <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" value="Nom">
+        <input type="text" id="nom" name="nom" placeholder="Nom">
     </div>
     <div class="input-group">
         <label for="telephone">Téléphone :</label>
-        <input type="text" id="telephone" name="telephone" value="Téléphone">
+        <input type="text" id="telephone" name="telephone" placeholder="Téléphone">
     </div>
     <div class="input-group">
         <label for="pays">Pays :</label>
-        <input type="text" id="pays" name="pays" value="Pays">
+        <input type="text" id="pays" name="pays" placeholder="Pays">
     </div>
     <div class="input-group">
         <label for="adresse">Adresse :</label>
-        <input type="text" id="adresse" name="adresse" value="adresse">
+        <input type="text" id="adresse" name="adresse" placeholder="adresse">
     </div>
     <div class="input-group">
         <label for="email">Email :</label>
-        <input type="text" id="email" name="email" value="email">
+        <input type="text" id="email" name="email" placeholder="email">
     </div>
     <div class="input-group">
         <label for="competence">Compétence :</label>
-        <textarea id="competence" name="competence" wrap="soft"></textarea>
+        <textarea id="competence" name="competence" wrap="soft" placeholder="Compétence"></textarea>
     </div>
     <div class="input-group">
         <label for="experience">Experience :</label>
-        <textarea id="experience" name="experience" wrap="soft"></textarea>
+        <textarea id="experience" name="experience" wrap="soft" placeholder="Expérience"></textarea>
     </div>
     <div class="input-group">
         <label for="ville">Ville :</label>
-        <input type="text" id="ville" name="ville" value="Ville">
+        <input type="text" id="ville" name="ville" placeholder="Ville">
     </div>
     <p>Vous avez déjà envoyer un CV ?</p>
     <div class="input-group">
